f51faebd07b95624a23712eb423f3a0ec81f1edf
Stephan Deininghaus <stephan@deininghaus.com>
2023-02-05 00:42:32 +0100

    Add read/write primitive methods to Table

--- src/Fugu.Core/IO/Table.cs
+++ src/Fugu.Core/IO/Table.cs
@@ -1,5 +1,16 @@
-﻿namespace Fugu.Core.IO;
+﻿using System.Buffers;
+
+namespace Fugu.Core.IO;
 
 public class Table
 {
+    public ReadOnlySpan<byte> GetSpan(long start, int length)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IBufferWriter<byte> GetWriter()
+    {
+        throw new NotImplementedException();
+    }
 }
