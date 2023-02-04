8aeec9968103f4b1becd7a4735eba675c116208d
Stephan Deininghaus <stephan@deininghaus.com>
2023-02-05 00:04:57 +0100

    Add instance methods for actors in KeyValueStore

---
+++ src/Fugu.Core/Actors/Messages/DummyMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace Fugu.Core.Actors.Messages;
+
+public class DummyMessage
+{
+    // TODO: This is only a placeholder during prototyping, replace with actual message definitions!
+}
--- src/Fugu.Core/KeyValueStore.cs
+++ src/Fugu.Core/KeyValueStore.cs
@@ -1,10 +1,30 @@
-﻿namespace Fugu.Core;
+﻿using Fugu.Core.Actors;
+
+namespace Fugu.Core;
 
 public sealed class KeyValueStore : IAsyncDisposable
 {
-    private KeyValueStore()
-    {
+    private readonly AllocationActor _allocationActor;
+    private readonly WriterActor _writerActor;
+    private readonly IndexActor _indexActor;
+    private readonly SnapshotsActor _snapshotsActor;
+    private readonly SegmentStatsActor _segmentStatsActor;
+    private readonly CompactionActor _compactionActor;
 
+    public KeyValueStore(
+        AllocationActor allocationActor,
+        WriterActor writerActor,
+        IndexActor indexActor,
+        SnapshotsActor snapshotsActor,
+        SegmentStatsActor segmentStatsActor,
+        CompactionActor compactionActor)
+    {
+        _allocationActor = allocationActor;
+        _writerActor = writerActor;
+        _indexActor = indexActor;
+        _snapshotsActor = snapshotsActor;
+        _segmentStatsActor = segmentStatsActor;
+        _compactionActor = compactionActor;
     }
 
     public static ValueTask<KeyValueStore> CreateAsync(TableSet tableSet)
