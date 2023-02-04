46d74512ac9854e5bcfed413c3414952cce19f01
Stephan Deininghaus <stephan@deininghaus.com>
2023-02-04 23:56:00 +0100

    Stub out top-level API on KeyValueStore

--- src/Fugu.Core/KeyValueStore.cs
+++ src/Fugu.Core/KeyValueStore.cs
@@ -1,5 +1,29 @@
 ﻿namespace Fugu.Core;
 
-public class KeyValueStore
+public sealed class KeyValueStore : IAsyncDisposable
 {
+    private KeyValueStore()
+    {
+
+    }
+
+    public static ValueTask<KeyValueStore> CreateAsync(TableSet tableSet)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<Snapshot> GetSnapshotAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask WriteAsync(WriteBatch batch)
+    {
+        throw new NotImplementedException();
+    }
 }
