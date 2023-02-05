541ab94aa37659faf8ce08e6ed992d78933b9148
Stephan Deininghaus <stephan@deininghaus.com>
2023-02-05 12:03:46 +0100

    Add TableReader and TableWriter types

---
+++ src/Fugu.Core/IO/TableWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Fugu.Core.IO;
+
+public class TableWriter
+{
+    private readonly IBufferWriter<byte> _bufferWriter;
+
+    public TableWriter(IBufferWriter<byte> bufferWriter)
+	{
+        _bufferWriter = bufferWriter;
+    }
+
+    public void Write<T>(in T value) where T : struct
+    {
+        var size = Unsafe.SizeOf<T>();
+        var span = _bufferWriter.GetSpan(size);
+
+        MemoryMarshal.Write(span, ref Unsafe.AsRef(in value));
+        _bufferWriter.Advance(size);
+    }
+
+    public void WriteBytes(ReadOnlySpan<byte> value)
+    {
+        _bufferWriter.Write(value);
+    }
+}
