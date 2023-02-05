2437754de24a886d13a9543683a4bbde97caa2f9
Stephan Deininghaus <stephan@deininghaus.com>
2023-02-05 12:26:20 +0100

    Add examples/BasicDemo project

---
+++ examples/BasicDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using Fugu.Core;
+
+var tableSet = new TableSet();
+
+await using (var store = await KeyValueStore.CreateAsync(tableSet))
+{
+    var batch = new WriteBatch();
+    await store.WriteAsync(batch);
+}
