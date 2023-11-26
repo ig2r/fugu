d5201ac77c15bef21e7e344670eb92daa4486b80
Stephan Deininghaus <stephan@deininghaus.com>
2023-11-26 11:47:50 +0100

    Add Fugu.Core.Tests xUnit project

---
+++ tests/Fugu.Core.Tests/ChangeSetTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Fugu.Core.Tests;
+
+public class ChangeSetTests
+{
+    [Fact]
+    public void AddOrUpdate_CalledTwiceWithSameKey_ThrowsInvalidOperationException()
+    {
+        var changeSet = new ChangeSet();
+        changeSet.AddOrUpdate("foo"u8, Array.Empty<byte>());
+
+        var ex = Record.Exception(() => changeSet.AddOrUpdate("foo"u8, Array.Empty<byte>()));
+
+        Assert.IsAssignableFrom<InvalidOperationException>(ex);
+    }
+
+    [Fact]
+    public void Remove_KeyAlreadyTrackedUsingAddedOrUpdated_ThrowsInvalidOperationException()
+    {
+        var changeSet = new ChangeSet();
+        changeSet.AddOrUpdate("foo"u8, Array.Empty<byte>());
+
+        var ex = Record.Exception(() => changeSet.Remove("foo"u8));
+
+        Assert.IsAssignableFrom<InvalidOperationException>(ex);
+    }
+}
