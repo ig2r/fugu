0bddd1bb45e0a79a1b8045db592e01932c5bf1e9
Stephan Deininghaus <stephan@deininghaus.com>
2023-11-26 12:00:36 +0100

    Add test to exercise full write and read paths

---
+++ tests/Fugu.Core.Tests/KeyValueStoreTests.cs
@@ -0,0 +1,26 @@
+﻿using Fugu.IO;
+using System.Text;
+
+namespace Fugu.Core.Tests;
+
+public class KeyValueStoreTests
+{
+    [Fact]
+    public async Task KeyValueStore_SavedChanges_CanBeReadFromSnapshot()
+    {
+        var storage = new InMemoryStorage();
+        await using var store = await KeyValueStore.CreateAsync(storage);
+
+        var changeSet = new ChangeSet
+        {
+            ["foo"u8] = Encoding.UTF8.GetBytes("Hello, world"),
+        };
+
+        await store.SaveAsync(changeSet);
+
+        using var snapshot = await store.GetSnapshotAsync();
+        var retrievedValue = await snapshot.ReadAsync("foo"u8);
+
+        Assert.Equal("Hello, world", Encoding.UTF8.GetString(retrievedValue.ToArray()));
+    }
+}
