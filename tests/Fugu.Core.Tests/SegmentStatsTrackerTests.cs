385a3fe663d2e37bd6c4dc7f8832fad2332baca8
Stephan Deininghaus <stephan@deininghaus.com>
2024-01-28 23:03:52 +0100

    Update SegmentStatsTrackerTests

--- tests/Fugu.Core.Tests/SegmentStatsTrackerTests.cs
+++ tests/Fugu.Core.Tests/SegmentStatsTrackerTests.cs
@@ -5,47 +5,27 @@ namespace Fugu.Core.Tests;
 
 public class SegmentStatsTrackerTests
 {
-    //[Fact]
-    //public async Task OnPayloadAdded_DoesNotImmediatelyReflectInStats()
-    //{
-    //    var tracker = new SegmentStatsTracker();
+    [Fact]
+    public async Task Add_SegmentWithSinglePayload_ReflectsPayloadInStats()
+    {
+        // Arrange tracker and some moving bits we need to make it track a (fictional) payload
+        var tracker = new SegmentStatsTracker();
 
-    //    var storage = new InMemoryStorage();
-    //    var slab = await storage.CreateSlabAsync();
-    //    var segment = new Segment(1, 1, slab);
-    //    var payload = new KeyValuePair<byte[], SlabSubrange>("foo"u8.ToArray(), new SlabSubrange(10, 23));
+        var storage = new InMemoryStorage();
+        var slab = await storage.CreateSlabAsync();
+        var segment = new Segment(1, 1, slab);
+        var builder = new SegmentStatsBuilder(segment);
 
-    //    tracker.OnPayloadAdded(segment, payload);
+        var payload = new KeyValuePair<byte[], SlabSubrange>("foo"u8.ToArray(), new SlabSubrange(10, 23));
+        builder.OnPayloadAdded(payload);
 
-    //    var statsBySegment = tracker.ToImmutable();
+        tracker.Add(builder);
 
-    //    Assert.Empty(statsBySegment);
-    //}
+        var stats = tracker.ToImmutable();
+        var singleStatsItem = Assert.Single(stats);
 
-    //[Fact]
-    //public async Task OnPayloadAdded_SwitchingToNewOutputSegment_PreviousOutputSegmentReflectsInStats()
-    //{
-    //    var tracker = new SegmentStatsTracker();
-
-    //    var storage = new InMemoryStorage();
-    //    var slab = await storage.CreateSlabAsync();
-    //    var segment1 = new Segment(1, 1, slab);
-    //    var payload1 = new KeyValuePair<byte[], SlabSubrange>("foo"u8.ToArray(), new SlabSubrange(10, 23));
-    //    var segment2 = new Segment(2, 2, slab);
-    //    var payload2 = new KeyValuePair<byte[], SlabSubrange>("bar"u8.ToArray(), new SlabSubrange(10, 42));
-
-    //    tracker.OnPayloadAdded(segment1, payload1);
-    //    tracker.OnPayloadAdded(segment2, payload2);
-
-    //    var statsBySegment = tracker.ToImmutable();
-
-    //    // There should now be a single entry in the tracker for the previous output segment
-    //    var singleSegmentStats = Assert.Single(statsBySegment);
-    //    Assert.Equal(segment1, singleSegmentStats.Key);
-
-    //    // The entire combined size of key + value for the payload should count against live bytes
-    //    Assert.Equal(0, singleSegmentStats.Value.StaleBytes);
-    //    Assert.Equal(3 + 23, singleSegmentStats.Value.LiveBytes);
-    //    Assert.Equal(3 + 23, singleSegmentStats.Value.TotalBytes);
-    //}
+        Assert.Same(segment, singleStatsItem.Key);
+        Assert.Equal(payload.Key.Length + payload.Value.Length, singleStatsItem.Value.LiveBytes);
+        Assert.Equal(0, singleStatsItem.Value.StaleBytes);
+    }
 }
