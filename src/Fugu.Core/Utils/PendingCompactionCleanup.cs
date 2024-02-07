7b0df702543fbe90e196565d44feccd029b9701b
Stephan Deininghaus <stephan@deininghaus.com>
2024-02-07 22:26:21 +0100

    Defer compaction cleanup until no snapshots can see old state

---
+++ src/Fugu.Core/Utils/PendingCompactionCleanup.cs
@@ -0,0 +1,13 @@
+﻿namespace Fugu.Utils;
+
+/// <summary>
+/// Remaining cleanup activities to be performed after a compaction.
+/// </summary>
+/// <param name="CompactionClock">The compaction clock component after which the cleanup can be performed.</param>
+/// <param name="Segments">The source segments for the compaction, to be removed from the store.</param>
+/// <param name="CapacityChange">The net change in total store capacity that resulted from the compaction.</param>
+internal record PendingCompactionCleanup(
+    long CompactionClock,
+    Segment[] Segments,
+    long CapacityChange
+);
