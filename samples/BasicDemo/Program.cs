e437dc8fbeccd033d75bd4ea4921d7a7b2bab8cd
Stephan Deininghaus <ig2r@users.noreply.github.com>
2024-05-14 21:46:19 +0200

    Make basic example way more basic (#37)
    Drastically simplifies the "basic" example, as some of the more involved
    scenarios are now covered by the throughput and dependency injection
    demonstrator projects.

--- samples/BasicDemo/Program.cs
+++ samples/BasicDemo/Program.cs
@@ -2,44 +2,21 @@
 using Fugu.IO;
 using System.Text;
 
-const int Iterations = 10000;
-const int KeyspaceSize = 200;
-
+// Create a new key-value store instance that will keep its data in memory.
 var storage = new InMemoryStorage();
+await using var store = await KeyValueStore.CreateAsync(storage);
 
-await using (var store = await KeyValueStore.CreateAsync(storage))
+// Write some data to the store in an atomic transaction.
+var changeSet = new ChangeSet
 {
-    var random = new Random();
+    ["greeting"u8] = Encoding.UTF8.GetBytes("Hello, world!"),
+};
 
-    for (var i = 0; i < Iterations; i++)
-    {
-        var payloadNo = random.Next(KeyspaceSize);
-        var tombstoneNo = random.Next(KeyspaceSize);
+await store.SaveAsync(changeSet);
 
-        var key = Encoding.UTF8.GetBytes($"key:{payloadNo}");
-        var changeSet = new ChangeSet
-        {
-            [key] = Encoding.UTF8.GetBytes($"value:{i}"),
-        };
-
-        if (payloadNo != tombstoneNo)
-        {
-            var tombstone = Encoding.UTF8.GetBytes($"key:{tombstoneNo}");
-            changeSet.Remove(tombstone);
-        }
-
-        await store.SaveAsync(changeSet);
-    }
-
-    Console.WriteLine("Writing completed, pausing for 1 second");
-    await Task.Delay(TimeSpan.FromSeconds(1));
-
-    using (var snapshot = await store.GetSnapshotAsync())
-    {
-        foreach (var key in snapshot.Keys)
-        {
-            var value = await snapshot.ReadAsync(key.ToArray());
-            Console.WriteLine($"Key: {Encoding.UTF8.GetString(key.ToArray())} - {Encoding.UTF8.GetString(value.Span)}");
-        }
-    }
+// Read back the data we just wrote and print it to the console.
+using (var snapshot = await store.GetSnapshotAsync())
+{
+    var value = await snapshot.ReadAsync("greeting"u8);
+    Console.WriteLine(Encoding.UTF8.GetString(value.Span));
 }
